2b6fa3d3c858f4ea22be871f549257cbe58bd67d
ThomasKlein18 <thomasklein18@gmail.com>
2019-06-27 10:19:41 +0200

    works for up to 1000 images

--- main.py
+++ main.py
@@ -14,44 +14,71 @@ def num_to_binary(num, digits):
     #return list('{0:02b}'.format(num))
     return list(str('{0:0'+str(digits)+'b}').format(num))
 
-
+def binary_estimates_to_num(binary):
+    return np.sum([j * 2**i for i,j in enumerate(reversed(np.round(binary)))])
 
 if __name__ == "__main__":
 
     ## paths ##
     storage_path = "../"
-    plot_path = storage_path + "plots/"
-    weight_path = storage_path + "weights/"
+    version = "v0/"
+    plot_path = storage_path + version + "plots/"
+    weight_path = storage_path + version + "weights/"
 
+
+   
+
+    """
+    A remark on the loss: 
+    sparse crossentropy just means: We get the label as an integer 
+    between 0 and 9. categorical crossentropy means that we expect
+    one-hot encoded labels.
+
+    If you use categorical crossentropy to train on memorized labels, 
+    all of the outputs just go to 1, so that is why you see a stagnating loss
+    and a slightly larger accuracy (due to the way accuracy is calculated.)
+    """
     ## parameters ##
-    max_samples = 2
-    out_size = 2
+    used_loss = 'mean_squared_error' #'categorical_crossentropy'
+    max_samples = 1000
+    out_size = 16
+    enable_memorization = True
 
-    assert max_samples < 2**out_size, "Encoding {} samples into {} binary digits is not possible, limit is {}.".format(max_samples, out_size, 2**out_size)
+     ## network parameters ##
+    num_epochs = max_samples * 100
+    batchsize = 100
+
+    assert batchsize <= max_samples, "Batch size can't be bigger than number of samples."
+
+    # automatically adjusting out_size and loss function if we are not memorizing
+    out_size = 10 if not enable_memorization else out_size
+    #used_loss = 'sparse_categorical_crossentropy' if not enable_memorization else 'categorical_crossentropy'
+    if enable_memorization:
+        assert max_samples < 2**out_size, "Encoding {} samples into {} binary digits is not possible, limit is {}.".format(max_samples, out_size, 2**out_size)
 
     ## model definition ##
     model = tf.keras.Sequential()
     model.add(layers.Flatten())
     model.add(layers.Dense(128, activation='tanh', input_shape=(784,))) #starting with MNIST / fashionMNIST
-    #model.add(layers.Dense(64, activation='tanh'))
+    model.add(layers.Dense(64, activation='tanh'))
     model.add(layers.Dense(out_size, activation='sigmoid'))
 
     ## model setup ##
-    model.compile(optimizer=tf.keras.optimizers.SGD(),
-                loss='mean_squared_error',
+    model.compile(optimizer=tf.train.AdagradOptimizer(0.005),
+                loss=used_loss,
                 metrics=['accuracy'])
 
-    ## get dataset
+    # getting the dataset
     fashion_mnist = tf.keras.datasets.fashion_mnist
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+    # only using the first max_samples samples
     train_images = train_images[0:max_samples,:,:]
     train_labels = train_labels[0:max_samples]
 
-    # replacing labels with numbers
-    train_labels = np.array([num_to_binary(i,out_size) for i in range(len(train_labels))])
-
-    
+    if enable_memorization :
+        # replacing labels with numbers
+        train_labels = np.array([num_to_binary(i,out_size) for i in range(len(train_labels))])
 
 
     callbacks = [
@@ -59,15 +86,22 @@ if __name__ == "__main__":
     tf.keras.callbacks.TensorBoard(log_dir=plot_path)
     ]
 
-
     model.fit(train_images,
-                train_labels,
-            epochs=10000,
-            batch_size=1,
+            train_labels,
+            epochs=num_epochs,
+            batch_size=batchsize,
             #validation_data=(test_images,test_labels),
             #validation_steps=100,
             callbacks = callbacks)
 
     model.save_weights(weight_path)
 
-    #TODO: print some model predictions, see what happens
+    positives = 0
+    for i in range(max_samples):
+        test_img = train_images[i,:,:]
+        test_img = np.reshape(test_img, [1,28,28])
+        pred = model.predict(test_img, batch_size=1)[0]
+        
+        if(binary_estimates_to_num(pred) == i):
+            positives += 1
+    print("Result:",positives/max_samples)
